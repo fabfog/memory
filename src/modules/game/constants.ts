9aa26f25f2cd720fcddbdbbb336d9801a3e6064f
Fabio Fognani <fabio.fognani@gmail.com>
2023-05-06 16:12:38 +0200

    refactor: moved constants into env file

--- src/modules/game/constants.ts
+++ src/modules/game/constants.ts
@@ -1,12 +1,20 @@
-export const DEFAULT_CARD_WIDTH = 100;
-export const DEFAULT_CARD_HEIGHT = 150;
+export const DEFAULT_CARD_WIDTH: number =
+  +process.env.NEXT_PUBLIC_DEFAULT_CARD_WIDTH!;
+export const DEFAULT_CARD_HEIGHT: number =
+  +process.env.NEXT_PUBLIC_DEFAULT_CARD_HEIGHT!;
 
-export const DEFAULT_BOARD_WIDTH = 3;
-export const DEFAULT_BOARD_HEIGHT = 4;
+export const DEFAULT_BOARD_WIDTH: number =
+  +process.env.NEXT_PUBLIC_DEFAULT_BOARD_WIDTH!;
+export const DEFAULT_BOARD_HEIGHT: number =
+  +process.env.NEXT_PUBLIC_DEFAULT_BOARD_HEIGHT!;
 
-export const MIN_BOARD_WIDTH = 2;
-export const MAX_BOARD_WIDTH = 6;
-export const MIN_BOARD_HEIGHT = 2;
-export const MAX_BOARD_HEIGHT = 6;
+export const MIN_BOARD_WIDTH: number =
+  +process.env.NEXT_PUBLIC_MIN_BOARD_WIDTH!;
+export const MAX_BOARD_WIDTH: number =
+  +process.env.NEXT_PUBLIC_MAX_BOARD_WIDTH!;
+export const MIN_BOARD_HEIGHT: number =
+  +process.env.NEXT_PUBLIC_MIN_BOARD_HEIGHT!;
+export const MAX_BOARD_HEIGHT: number =
+  +process.env.NEXT_PUBLIC_MAX_BOARD_HEIGHT!;
 
-export const LS_OPTIONS_KEY = "gameOptions";
+export const LS_OPTIONS_KEY = process.env.NEXT_PUBLIC_LS_OPTIONS_KEY!;
