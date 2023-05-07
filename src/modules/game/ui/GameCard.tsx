158d2560f98ab310ef701fdf773cf28ce7cd09bf
Fabio Fognani <fabio.fognani@gmail.com>
2023-05-07 10:42:31 +0200

    style: minor aesthetic improvements

--- src/modules/game/ui/GameCard.tsx
+++ src/modules/game/ui/GameCard.tsx
@@ -4,6 +4,7 @@ import {
   DEFAULT_CARD_HEIGHT,
   DEFAULT_CARD_WIDTH,
 } from "@/modules/game/constants";
+import { PawIcon } from "@/modules/common/ui/icons/PawIcon";
 
 export interface GameCardProps {
   value: number;
@@ -31,9 +32,9 @@ export const GameCard: FC<GameCardProps> = ({
     <div
       className={`
         w-16 h-24 sm:w-24 sm:h-36
-        rounded-xl bg-gradient-to-b border-4 border-slate-400 from-primary to-slate-800 ${
-          disabled ? "opacity-75" : ""
-        } ${className ?? ""}`}
+        rounded-xl overflow-hidden
+        border-4 border-slate-500
+        ${disabled ? "opacity-75" : ""} ${className ?? ""}`}
       data-test={value}
       style={{
         borderColor: !flipped
@@ -46,6 +47,12 @@ export const GameCard: FC<GameCardProps> = ({
               DEFAULT_CARD_HEIGHT + value
             }?image=${value})`,
       }}
-    />
+    >
+      {flipped && (
+        <div className="bg-gradient-to-b h-full from-primary to-slate-800 flex justify-center items-center">
+          <PawIcon className="w-16 h-16 fill-slate-400" />
+        </div>
+      )}
+    </div>
   );
 };
