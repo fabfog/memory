4495320fc86a0b1447596e9660027f78b23dd3e2
Fabio Fognani <fabio.fognani@gmail.com>
2023-05-07 17:51:07 +0200

    feat: added flip animation on cards

--- src/modules/game/ui/GameCard.tsx
+++ src/modules/game/ui/GameCard.tsx
@@ -19,25 +19,35 @@ export const GameCard: FC<GameCardProps> = ({
   className,
   disabled,
 }) => {
+  const commonClasses = `
+    w-20 h-32 sm:w-24 h-36
+    rounded-xl overflow-hidden
+    ${flipped ? "border-4 border-primary" : ""}
+    ${disabled ? "opacity-75" : ""}
+    ${className ?? ""}
+  `;
   return (
-    <div
-      className={`
-        w-20 h-32 sm:w-24 h-36
-        rounded-xl overflow-hidden bg-cover
-        ${flipped ? "border-4 border-primary" : ""}
-        ${disabled ? "opacity-75" : ""}
-        ${className ?? ""}`}
-      style={{
-        backgroundImage: flipped
-          ? undefined
-          : `url(https://placekitten.com/${DEFAULT_CARD_WIDTH}/${DEFAULT_CARD_HEIGHT}?image=${value})`,
-      }}
-    >
-      {flipped && (
-        <div className="bg-gradient-to-br h-full from-secondary to-neutral flex justify-center items-center">
-          <PawIcon className="w-10 sm:w-16 h-10 sm:h-16 fill-primary" />
-        </div>
-      )}
-    </div>
+    <label className={`swap swap-flip ${flipped ? "swap-active" : ""}`}>
+      <div
+        className={`
+          ${commonClasses}
+          swap-on
+          bg-gradient-to-br from-secondary to-neutral 
+          flex justify-center items-center
+        `}
+      >
+        <PawIcon className="w-10 sm:w-16 h-10 sm:h-16 fill-primary" />
+      </div>
+      <div
+        className={`
+          ${commonClasses}
+          swap-off
+          bg-cover
+        `}
+        style={{
+          backgroundImage: `url(https://placekitten.com/${DEFAULT_CARD_WIDTH}/${DEFAULT_CARD_HEIGHT}?image=${value})`,
+        }}
+      />
+    </label>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,21 @@ import Link from "next/link";
 
 import { GameCard } from "@/modules/game/ui/GameCard";
 import { PawIcon } from "@/modules/common/ui/icons/PawIcon";
+import { useState } from "react";
+import { useInterval } from "react-use";
+
+const displayedCards = 4;
+
+// the order in which the cards are flipped
+// simulates a "clockwise" flipping animation
+const cardsFlipIndexSequence = [0, 1, 3, 2];
 
 export default function Home() {
+  const [flipIndex, setFlipIndex] = useState(0);
+  useInterval(() => {
+    setFlipIndex((i) => (i + 1) % displayedCards);
+  }, 1000);
+
   return (
     <MainLayout>
       <div className="text-2xl mt-8 uppercase animate-bounce text-center mx-auto">
@@ -13,12 +26,12 @@ export default function Home() {
 
       <div className="flex flex-col gap-4">
         <div className="grid grid-cols-2 gap-4 max-w-fit mx-auto mb-8">
-          {new Array(4).fill(0).map((_, i) => (
+          {new Array(displayedCards).fill(0).map((_, i) => (
             <GameCard
-              className="w-24 h-36 sm:w-32 sm:h-48"
-              flipped={i % 3 === 0}
-              value={i}
               key={i}
+              className="w-24 h-36 sm:w-32 sm:h-48"
+              flipped={i !== cardsFlipIndexSequence[flipIndex]}
+              value={i}
             />
           ))}
         </div>
